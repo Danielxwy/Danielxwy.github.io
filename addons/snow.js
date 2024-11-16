6ee52bf75c7263e6d3569aa858c1760d994a5da5
Danielxwy <1907610413@QQ.com>
2024-11-16 22:21:59 +0800

    Site updated: 2024-11-16 22:21:55

--- addons/snow.js
+++ addons/snow.js
@@ -8,10 +8,24 @@
         speed: 0.2,
         opacity: 0.3,
         stepsize: .1,
+        fadeInSpeed: 0.02,// 越大越快
+        fadeOutSpeed: 0.02
     };
 
     // 记录下雪状态
     let isSnowing = true;
+
+    document.addEventListener("OnSnowStateChange", (event) => {
+        const lastState = isSnowing;
+        //重新开始
+        if (!lastState && event.detail.enable === "true") {
+            isSnowing = true;
+            startSnow();
+            fadeInCanvas();
+        } else if (lastState && event.detail.enable === "false") {
+            fadeOutCanvas();
+        }
+    });
 
     const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(callback){window.setTimeout(callback, 1000/60);};
     window.requestAnimationFrame = requestAnimationFrame;
@@ -22,6 +36,34 @@
     let flakes = [];
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    let fadeOutOpacity = 1.0;
+    const fadeOutCanvas = () => {
+        if (fadeOutOpacity > 0) {
+            ctx.globalAlpha = fadeOutOpacity;
+            fadeOutOpacity -= snowConf.fadeOutSpeed; // 调整步长以控制缓动速度
+            requestAnimationFrame(fadeOutCanvas);
+        } else {
+            isSnowing = false;
+            // 清空画布
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.globalAlpha = 1.0;
+            fadeOutOpacity = 1.0; // 重置
+        }
+    };
+
+    let fadeInOpacity = 0;
+    const fadeInCanvas = () => {
+        if (fadeInOpacity < 1) {
+            ctx.globalAlpha = fadeInOpacity;
+            fadeInOpacity += snowConf.fadeInSpeed; // 调整步长以控制缓动速度
+            requestAnimationFrame(fadeInCanvas);
+        } else {
+            ctx.globalAlpha = 1.0;
+            fadeInOpacity = 0; // 重置
+        }
+    };
+
     const snow = () => {
         if (!isSnowing) {
             return; // 结束
