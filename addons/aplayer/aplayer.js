2e70a366690d08cda342310c8380a693f5856c2f
Danielxwy <1907610413@QQ.com>
2024-11-23 02:05:11 +0800

    Site updated: 2024-11-23 02:05:08

--- addons/aplayer/aplayer.js
+++ addons/aplayer/aplayer.js
@@ -1,5 +1,10 @@
 const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
-  audio: [], // 这里加载您的音频配置
+  audio: [{
+    name: 'persona', // 音频名称
+    artist: 'Xeuphoria,OneChilledPanda', // 音频作者
+    url: '../../bgm/persona - Xeuphoria,OneChilledPanda.mp3', // 音频地址
+    cover: '../../bgm/cover.jpg'
+  }], // 这里加载您的音频配置
 });
