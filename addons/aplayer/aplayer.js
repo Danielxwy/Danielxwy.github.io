85531416686a1e12b22f1f508a7255e413debe6f
Danielxwy <1907610413@QQ.com>
2024-11-27 21:52:38 +0800

    Site updated: 2024-11-27 21:52:30

--- addons/aplayer/aplayer.js
+++ addons/aplayer/aplayer.js
@@ -1,15 +1,18 @@
 const ap = new APlayer({
   container: document.getElementById('aplayer'),
   fixed: true,
-  audio: [{
-    name: '旅人の唄', // 音频名称
-    artist: '大原ゆい子', // 音频作者
-    url: '../../bgm/旅人の唄 - 大原ゆい子.mp3',
-    cover: '../../bgm/cover.webp'
-  }, {
-    name: '目覚めの唄',
-    artist: '大原ゆい子',
-    url: '../../bgm/目覚めの唄 - 大原ゆい子.mp3',
-    cover: '../../bgm/cover.webp'
-  }], // 这里加载您的音频配置
+  volume: 0.2,
+  audio: [
+    {
+      name: '旅人の唄',
+      artist: '大原ゆい子',
+      url: '../../bgm/旅人の唄 - 大原ゆい子.mp3',
+      cover: '../../bgm/cover.webp'
+    }, {
+      name: '目覚めの唄',
+      artist: '大原ゆい子',
+      url: '../../bgm/目覚めの唄 - 大原ゆい子.mp3',
+      cover: '../../bgm/cover.webp'
+    }
+  ], // 这里加载您的音频配置
 });
